513259b3bd4e162d0f3881868b36d49015389843
Stupzz <m.carbonnier73@gmail.com>
2021-07-16 18:57:27 +0200

    first commit

---
+++ src/main/java/com/stupzz/trainingbook/TrainingBookApplication.java
@@ -0,0 +1,13 @@
+package com.stupzz.trainingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingBookApplication.class, args);
+    }
+
+}
