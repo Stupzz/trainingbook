8d8f0939eff14ed99f8844ea5c43ce1d2283be9a
Stupzz <m.carbonnier73@gmail.com>
2021-07-18 19:44:23 +0200

    Adding custom message for exception

--- src/main/java/com/stupzz/trainingbook/Exceptions/NotFound/NotFoundException.java
+++ src/main/java/com/stupzz/trainingbook/Exceptions/NotFound/NotFoundException.java
@@ -3,7 +3,7 @@ package com.stupzz.trainingbook.Exceptions.NotFound;
 import com.stupzz.trainingbook.Exceptions.ServerError;
 import org.springframework.http.HttpStatus;
 
-public abstract class NotFoundException extends ServerError {
+public class NotFoundException extends ServerError {
     public NotFoundException(String message) {
         super(message);
     }
